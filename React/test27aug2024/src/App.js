a0667dd539653cd3f7697497b2fb1d10dc9f4aa3
P Santhosh <ssanthosh3117@gmail.com>
2024-08-27 14:30:55 +0530

    update

--- React/test27aug2024/src/App.js
+++ React/test27aug2024/src/App.js
@@ -5,17 +5,20 @@ function App() {
   const [name,setName] = useState('')
   const click = async() => {
     try{
-      const response = await fetch(`https://api.agify.io/?name=${name}`);
+      const response = await fetch(`https://api.restful-api.dev/objects?name=${name}`);
       const data = await response.json();
       console.log(data)
+      for(let a in data){
+        console.log(a)
+      }
     } catch(error){
       console.error('Error fetching the data:',error);
     }
   };
+  
   return (
-    <div>
-      <input type='text' value={name} onChange={(e)=>setName(e.target.value)}placeholder='Enter your name'/>
-      <button onClick={click}>Find Meaning</button>
+    <div className="App">
+      <button onClick={click}>Get name from API</button>      
     </div>
   );
 }
