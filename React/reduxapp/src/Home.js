cf9c0ea87ccd213c2366be962dbc91085f4d76fe
P Santhosh <ssanthosh3117@gmail.com>
2024-08-28 19:26:32 +0530

    update

--- React/reduxapp/src/Home.js
+++ React/reduxapp/src/Home.js
@@ -1,6 +1,32 @@
-function Home(){
-    return(
-        <h1>This is Home page</h1>
-    )
-}
+import { useDispatch, useSelector } from "react-redux";
+import Header from './Header';
+
+const Home = () => {
+    const countVal = useSelector((state) => state.counter);
+    const dispatch = useDispatch();
+
+    const add = () => {
+        dispatch({
+            type: 'add',
+        })
+    };
+    
+    const sub = () => {
+        dispatch({
+            type: 'sub',
+        })
+    };
+
+    return (
+        <div >
+            <Header/>
+            <h1>This is the home page</h1>
+            <h1> Your Counter value: {countVal}</h1>
+            <input type="button" value="add"  onClick={add}/>
+            <input type="button" value="sub"  onClick={sub}/>
+        </div>
+    );
+    
+};
+
 export default Home;
--- React/reduxapp/src/redux/config.js
+++ React/reduxapp/src/redux/config.js
@@ -1,14 +1,20 @@
-import { configureStore } from '@reduxjs/toolkit'
-const newReducer = (state=0,action)=>{
+import { configureStore } from "@reduxjs/toolkit";
 
+const counterLogic = (state = 0, action) => {
+    switch(action.type) {
+        case "add":
+            return state + 1;
+        case "sub":
+            return state - 1;
+        default:
+            return state;
+    }
+    return state;
 }
 
 export const myStore = configureStore({
-    
-    reducer:{
-        "counter":newReducer,
-        "country":newReducer
-        
+    reducer: {
+        "counter": counterLogic
+        // you can add more code if you see fit
     }
-    
-})
+});
